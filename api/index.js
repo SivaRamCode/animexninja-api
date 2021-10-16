4eb4d52986bb84a5896e2d1bca4d520e4b326e70
SivaRamCode <67477195+SivaRamCode@users.noreply.github.com>
2021-10-16 18:20:41 +0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -7,7 +7,7 @@ const port = 5000;
 
 app.use(cors());
 
-const baseURL = "https://gogoanime.pe/";
+const baseURL = "https://www1.gogoanime.ai/";
 
 app.get("/api/home", (req, res) => {
   let info = {
